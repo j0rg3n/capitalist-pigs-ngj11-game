652d5ac56a188908c86cb40c2ca1c8d02a4740a8
j0rg3n <j0rg3n@gmail.com>
2011-01-29 17:30:30 +0000

    Pie chart thing.

---
+++ IndieExtinction/Assets/Scripts/PieChartGUIBehavior.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+using System.Collections;
+
+public class PieChartGUIBehavior : MonoBehaviour 
+{
+    public float size = 128f;
+    public Vector2 alignment = new Vector2(.5f, .5f);
+    public Texture2D rightBackground;
+    public Texture2D right;
+    public float amount = 0;
+
+    void OnGUI()
+    {
+        amount += Time.deltaTime * .25f;
+
+        var wrappedAmount = amount - Mathf.Floor(amount);
+
+        var camera = GlobalObjects.GetMainCamera();
+        var screenPos = camera.WorldToScreenPoint(transform.position);
+
+        var clockRect = new Rect(screenPos.x - (size - size * alignment.x),
+            screenPos.y - (size - size * alignment.y),
+            size * alignment.x,
+            size * alignment.y);
+        
+        if (wrappedAmount < .5f)
+        {
+            DrawRotatedTexture(clockRect, right, 0);
+            DrawRotatedTexture(clockRect, rightBackground, wrappedAmount);
+            DrawRotatedTexture(clockRect, rightBackground, .5f);
+        }
+        else if (wrappedAmount >= .5f)
+        {
+            DrawRotatedTexture(clockRect, right, .5f);
+            DrawRotatedTexture(clockRect, rightBackground, wrappedAmount);
+            DrawRotatedTexture(clockRect, right, 0);
+        }
+    }
+
+    private static void DrawRotatedTexture(Rect clockRect, Texture2D texture, float amount)
+    {
+        var center = new Vector2((clockRect.xMin + clockRect.xMax - 1) / 2,
+            (clockRect.yMin + clockRect.yMax - 1) / 2);
+
+        Matrix4x4 prevTransform = GUI.matrix;
+        GUIUtility.RotateAroundPivot(amount * 360, center);
+        GUI.DrawTexture(clockRect, texture, ScaleMode.StretchToFill, true, 0);
+        GUI.matrix = prevTransform;
+    }
+}
