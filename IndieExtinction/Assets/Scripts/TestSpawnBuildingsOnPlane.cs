10d421158a7597b93c353319885005b5aff76535
j0rg3n <j0rg3n@gmail.com>
2011-01-29 15:09:31 +0000

    Add: Grid-to-worldspace mapping functions.

---
+++ IndieExtinction/Assets/Scripts/TestSpawnBuildingsOnPlane.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using System.Collections;
+
+public class TestSpawnBuildingsOnPlane : MonoBehaviour 
+{
+    public Transform buildingPrefab;
+
+	void Start () 
+    {
+        int width = 5;
+        int height = 5;
+        for (int x = 0; x < width; ++x)
+        {
+            for (int y = 0; y < height; ++y)
+            {
+                var basePosition = MathUtil.GetWorldPositionFromGridCoordinate(GetComponent<MeshFilter>(), x, y, width, height);
+
+                Transform buildingTransform = (Transform)Instantiate(buildingPrefab);
+
+                var offset = buildingTransform.GetComponent<MeshFilter>().mesh.bounds.extents;
+                offset.Scale(Vector3.up);
+
+                basePosition = buildingTransform.InverseTransformPoint(basePosition);
+                buildingTransform.Translate(basePosition + offset);
+            }
+        }
+    }
+	
+	void Update () 
+    {
+	}
+}
+;
