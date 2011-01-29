1b0101b7b1bbf0549404174045f1ed2318cc499e
j0rg3n@gmail.com <j0rg3n@gmail.com@02508af9-65fe-245e-a445-d753007f0f16>
2011-01-29 11:15:17 +0000

    Add: IndieDev and Studio prefabs. Studio spawn behavior (controlled by click for now).

---
+++ trunk/IndieExtinction/Assets/Scripts/StudioBehavior.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+using System.Collections;
+
+public class StudioBehavior : MonoBehaviour 
+{
+    public Transform indieDevPrefab;
+
+    public void Start () 
+    {
+	}
+
+    public void OnMouseClicked(RaycastHit hitInfo)
+    {
+        var studioBounds = GetComponent<MeshFilter>().mesh.bounds;
+
+        for (int i = 0; i < 20; ++i)
+        {
+            var localSpawnPos = GetRandomPos(studioBounds);
+            var worldSpawnPos = transform.TransformPoint(localSpawnPos);
+
+            Transform indieDevInstance = (Transform)Instantiate(indieDevPrefab, worldSpawnPos, Quaternion.identity);
+        }
+
+        Destroy (gameObject);
+    }
+
+    private static Vector3 GetRandomPos(Bounds bounds)
+    {
+        float x = Random.RandomRange(bounds.center.x, bounds.center.x + bounds.extents.x);
+        float y = Random.RandomRange(bounds.center.x, bounds.center.x + bounds.extents.x);
+        float z = Random.RandomRange(bounds.center.x, bounds.center.x + bounds.extents.x);
+        return new Vector3(x, y, z);
+    }
+
+    public void Update() 
+    {
+	
+	}
+}
