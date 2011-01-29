fd6e66761de5286e573f91a76fed19e0c95893a5
j0rg3n <j0rg3n@gmail.com>
2011-01-29 02:01:03 +0000

    Fix: Billboard now points towards the camera.

---
+++ IndieExtinction/Assets/Scripts/ObjectNames.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using System.Collections;
+
+public class ObjectNames
+{
+    public const string MAIN_CAMERA = "Main Camera";
+
+    // Marks the class as 'static'. 
+    // Mono doesn't allow the 'static' modifier on classes.
+    private ObjectNames()
+    {
+    }
+}
