788dd3cf2416b85fb8680710c6a54ccf607629b4
j0rg3n@gmail.com <j0rg3n@gmail.com@02508af9-65fe-245e-a445-d753007f0f16>
2011-01-29 18:39:18 +0000

    Fix: Piechart y position was inverted. Add: Indie studio behavior with development and spawn.

--- trunk/IndieExtinction/Assets/Scripts/StudioBehavior.cs
+++ trunk/IndieExtinction/Assets/Scripts/StudioBehavior.cs
@@ -1,43 +1,15 @@
 using UnityEngine;
 using System.Collections;
 
-public class StudioBehavior : MonoBehaviour 
+public class StudioBehavior : StudioBehaviorBase
 {
-    public Transform indieDevFemalePrefab;
-    public Transform indieDevMalePrefab;
-    public AudioClip Sound_explotion;
-
-    public void Start () 
-    {
-	}
-
     public void OnMouseClicked(RaycastHit hitInfo)
-    {        
-        var studioBounds = GetComponent<MeshFilter>().mesh.bounds;
-
-        for (int i = 0; i < 20; ++i)
-        {
-            var localSpawnPos = RandomUtil.GetPointInBounds(studioBounds);
-            var worldSpawnPos = transform.TransformPoint(localSpawnPos);
-            //Transform indieDvInstance = (Transform)Instantiate(indieDevPrefab, worldSpawnPos, Quaternion.identity);
-            if (Random.value > 0.35)
-            {
-                Instantiate(indieDevMalePrefab, worldSpawnPos, Quaternion.identity);
-            }
-            else
-            {
-                Instantiate(indieDevFemalePrefab, worldSpawnPos, Quaternion.identity);
-            }
-        }
+    {
+        SpawnIndieDevs(20);
 
         audio.PlayOneShot(Sound_explotion);
         StartCoroutine(DestroyAfterDelay(Sound_explotion.length));
     }
-
-    public void Update() 
-    {
-	
-	}
 
     public IEnumerator DestroyAfterDelay(float seconds)
     {
