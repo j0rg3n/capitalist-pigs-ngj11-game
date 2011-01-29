f899dcc95f05e6380c5ff94fea1b497be9200445
j0rg3n@gmail.com <j0rg3n@gmail.com@02508af9-65fe-245e-a445-d753007f0f16>
2011-01-29 02:01:03 +0000

    Fix: Billboard now points towards the camera.

--- trunk/IndieExtinction/Assets/Scripts/BillboardBehavior.cs
+++ trunk/IndieExtinction/Assets/Scripts/BillboardBehavior.cs
@@ -1,19 +1,34 @@
 using UnityEngine;
 using System.Collections;
 
+/// <summary>
+/// Makes the given direction on the object point towards the screen.
+/// </summary>
 public class BillboardBehavior : MonoBehaviour 
 {
+    public Vector3 objectFrontVector = Vector3.up;
+
 	// Use this for initialization
 	void Start () 
     {
-	    
+        objectFrontVector.Normalize();   
 	}
 	
 	// Update is called once per frame
 	void Update () 
     {
-        // This doesn't work. Do the math, please.
-        var cam = GetComponent<Camera>();
-        transform.rotation.SetLookRotation(cam.transform.position);
+        var cam = GetMainCamera();
+
+        Vector3 toScreenVector = cam.transform.TransformDirection(Vector3.back);
+
+        var toScreenRotation = new Quaternion();
+        toScreenRotation.SetFromToRotation(objectFrontVector, toScreenVector);
+
+        transform.rotation = toScreenRotation;
+    }
+
+    private static Camera GetMainCamera()
+    {
+        return GameObject.Find(ObjectNames.MAIN_CAMERA).GetComponent<Camera>();
     }
 }
