c65f86a4e045cea9934426150dc3b65b73d2bccf
j0rg3n <j0rg3n@gmail.com>
2011-01-29 22:37:53 +0000

    Add: Theme music added to main scene, time remaining controlled by music.

--- IndieExtinction/Assets/Scripts/guiScriptBehavior.cs
+++ IndieExtinction/Assets/Scripts/guiScriptBehavior.cs
@@ -3,20 +3,20 @@ using System.Collections;
 
 public class guiScriptBehavior : MonoBehaviour 
 {
-        float startTime = 120.0f;
-        float timeRemaining = 0.0f;
-        string score = "Score: ";
-        string countDown;
-        int minutes;
-        int seconds;
+    string score = "Score: ";
+    string countDown;
+    int minutes;
+    int seconds;
+
     void OnGUI()
     {
-        timeRemaining= startTime - Time.time;
+        var globalgamestate = (GlobalGameStateBehavior)Object.FindObjectOfType(typeof(GlobalGameStateBehavior));
+
+        var timeRemaining = globalgamestate.TimeRemaining;
         minutes = ((int)timeRemaining / 60);
         seconds = ((int)timeRemaining % 60);
         countDown = minutes.ToString() + ":" + seconds.ToString("D2");
-        var globalgamestate = (GlobalGameStateBehavior)Object.FindObjectOfType(typeof(GlobalGameStateBehavior));
-        int cash = globalgamestate.getscore;
+        int cash = globalgamestate.Score;
         GUI.Label(new Rect(10, 10, 200, 20), score + cash);
         GUI.Label(new Rect(50, 50, 200, 20), countDown);
     }
