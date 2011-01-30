9a1cd2740470db9fb7cacf120b12664426f06a4c
j0rg3n@gmail.com <j0rg3n@gmail.com@02508af9-65fe-245e-a445-d753007f0f16>
2011-01-30 11:29:39 +0000

    Add: Persistent game state accross levels. Full game look (but not with death and victory screen). Fix: Robustness for the "main Camera" thing.

---
+++ trunk/IndieExtinction/Assets/Scripts/PersistentGameState.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Irrelevant.Assets.Scripts
+{
+    public enum Level
+    {
+        Intro    = 0,
+        Tutorial = 1,
+        Actual   = 2,
+        Death    = 3,
+    }
+
+    /// <summary>
+    /// Gamestate persisting between scenes.
+    /// </summary>
+    public sealed class PersistentGameState
+    {
+        public Level Level
+        {
+            get { return (Level)Application.loadedLevel; }
+        }
+
+        public void LoadDeath()
+        {
+            // TODO: Load death, not intro.
+            //Application.LoadLevel((int)Level.Death);
+            LoadIntro();
+        }
+
+        public void LoadIntro()
+        {
+            Application.LoadLevel((int) Level.Intro);
+        }
+
+        public void LoadNextLevel()
+        {
+            Level nextLevel = GetNextLevel(Level);
+            Application.LoadLevel((int) nextLevel);
+        }
+
+        public bool IsLastWave(int waveIndex)
+        {
+            return Level == Level.Tutorial && waveIndex == 2;
+        }
+
+        public int GetWaveSize(int waveIndex)
+        {
+            int[] levelWaveSizes = waveSizes[GetLevelIndex(Level)];
+            return levelWaveSizes[Math.Min(waveIndex, levelWaveSizes.Length - 1)];
+        }
+
+        public float GetWaveTime(int waveIndex)
+        {
+            float[] levelWaveTimes = waveTimes[GetLevelIndex(Level)];
+            return levelWaveTimes[Math.Min(waveIndex, levelWaveTimes.Length - 1)];
+        }
+
+        private static int GetLevelIndex(Level currentLevel)
+        {
+            return (int)currentLevel - (int)Level.Tutorial;
+        }
+
+        private static Level GetNextLevel(Level currentLevel)
+        {
+            switch (currentLevel)
+            {
+                case Level.Intro:
+                    return Level.Tutorial;
+                case Level.Tutorial:
+                    return Level.Actual;
+                case Level.Actual:
+                    // TODO: Victory screen!
+                    return Level.Intro;
+                default:
+                    return Level.Intro;
+            }
+        }
+
+        private int[][] waveSizes = new[] { 
+            new[] { 16, 32, },
+            new[] { 32, 32, 32, 32, 40 } 
+        };
+        
+        private float[][] waveTimes = new[] { 
+            new float[] { 16, 12, },
+            new float[] { 16, 12,  9,  8,  8 }
+        };
+    }
+}
